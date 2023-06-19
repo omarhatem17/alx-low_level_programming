522dfc0c971df036069266e290b03c813c210c15
omar_Hatem <omarhatem17@yahoo.com>
2023-06-19 12:34:54 -0700

    my

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char calpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	char salpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int a = 0;
+	int n = 0;
+		for (a = 0; a < 26; a++)
+		{
+			putchar(calpha[a]);
+		}
+		putchar('\n');
+	for (n = 0; a < 26; n++)
+	{
+		putchar(salpha[n]);
+	}
+	putchar('\n');
+return (0);
+}
+
