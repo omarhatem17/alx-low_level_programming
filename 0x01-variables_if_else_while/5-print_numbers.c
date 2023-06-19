779a1cec93251ef9ce23541754153330c43b6540
omar_Hatem <omarhatem17@yahoo.com>
2023-06-19 13:22:17 -0700

    ommmmr

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Prints numbers from 0 to 9.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int b = 0;
+
+	for (b = 0; b < 10; b++)
+	{
+		printf("%d\n", a[b]);
+	}
+	return (0);
+}
+
