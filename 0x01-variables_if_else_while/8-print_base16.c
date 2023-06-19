ab7c0bb87d660ef156007b48059d0533413178a2
omar_Hatem <omarhatem17@yahoo.com>
2023-06-19 13:50:00 -0700

    aa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the hexadecimal.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
