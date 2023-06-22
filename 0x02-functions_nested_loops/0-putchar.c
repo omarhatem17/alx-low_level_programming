a4b1d77f722c0b511e3f0f62773599e48451c7c3
omar_Hatem <omarhatem17@yahoo.com>
2023-06-21 18:05:42 -0700

    ff

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - prints all possible different combinations of three digits
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
